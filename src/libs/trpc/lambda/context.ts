1c809e3218a00a029c77511ebec851d0817f3033
cy948 <cy948@qq.com>
2025-06-20 16:19:23 +0800

    :rewind: revert: revert change

--- src/libs/trpc/lambda/context.ts
+++ src/libs/trpc/lambda/context.ts
@@ -2,7 +2,6 @@ import { parse } from 'cookie';
 import debug from 'debug';
 import { User } from 'next-auth';
 import { NextRequest } from 'next/server';
-import { ipAddress } from '@vercel/functions'
 
 import { JWTPayload, LOBE_CHAT_AUTH_HEADER, enableClerk, enableNextAuth } from '@/const/auth';
 import { oidcEnv } from '@/envs/oidc';
@@ -32,8 +31,6 @@ export interface AuthContext {
   resHeaders?: Headers;
   userAgent?: string;
   userId?: string | null;
-  // Add traces
-  ip?: string | null;
 }
 
 /**
@@ -48,7 +45,6 @@ export const createContextInner = async (params?: {
   oidcAuth?: OIDCAuth | null;
   userAgent?: string;
   userId?: string | null;
-  ip?: string | null;
 }): Promise<AuthContext> => {
   log('createContextInner called with params: %O', params);
   const responseHeaders = new Headers();
@@ -62,7 +58,6 @@ export const createContextInner = async (params?: {
     resHeaders: responseHeaders,
     userAgent: params?.userAgent,
     userId: params?.userId,
-    ip: params?.ip,
   };
 };
 
@@ -102,7 +97,6 @@ export const createLambdaContext = async (request: NextRequest): Promise<LambdaC
   let userId;
   let auth;
   let oidcAuth = null;
-  const ip = ipAddress(request);
 
   // Prioritize checking the standard Authorization header for OIDC Bearer Token validation
   if (oidcEnv.ENABLE_OIDC) {
@@ -138,7 +132,6 @@ export const createLambdaContext = async (request: NextRequest): Promise<LambdaC
           oidcAuth,
           ...commonContext,
           userId,
-          ip,
         });
       }
     } catch (error) {
